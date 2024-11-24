4328bd9bd6172041dbd797a68545204abb2e349a
PCalape <philipcalapezz@gmail.com>
2024-11-24 15:52:22 +0800

    fetch question

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthGuard } from './auth/auth-guard';
+import { QuestionDocument } from './questions.schema';
+import { isValidObjectId } from 'mongoose';
 
 @Controller()
 export class AppController {
@@ -8,5 +17,13 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('questions')
+  getQuestion(@Query('id') id: string): Promise<QuestionDocument> {
+    if (!isValidObjectId(id)) throw new BadRequestException('Invalid id');
+
+    return this.appService.getQuestion(id);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { Question, QuestionDocument } from './questions.schema';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @InjectModel(Question.name) private questionModel: Model<Question>,
+  ) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async getQuestion(id: string): Promise<QuestionDocument> {
+    return this.questionModel.findById(id);
   }
 }

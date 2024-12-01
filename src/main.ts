0deb4381dee6f4bba3b84760e1958e3cfb0788f0
PCalape <philipcalapezz@gmail.com>
2024-12-01 22:15:23 +0800

    allow from anywhere

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  app.use(
-    cors({
-      origin: '*',
-    }),
-  );
-  await app.listen(process.env.PORT ?? 3080);
+  app.enableCors();
+  await app.listen(process.env.PORT ?? 3080, '0.0.0.0');
 }
 bootstrap();
